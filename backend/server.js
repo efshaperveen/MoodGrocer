91a5c48b2a5e73bda479ba1130f694065e0ecd8c
efshaperveen <efshaperveen10@gmail.com>
2025-12-09 23:40:57 +0530

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,11 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin: process.env.CLIENT_URL
-// }));
-app.use(cors(
-));
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
+// app.use(cors(
+// ));
 
 
 // MOUNT ROUTES
